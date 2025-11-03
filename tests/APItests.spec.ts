63f45f9427f925700cc972c5066a7c8fb2d8370f
zrose49 <zxr110230@gmail.com>
2025-11-03 15:20:15 -0600

    added get product list api test

--- tests/APItests.spec.ts
+++ tests/APItests.spec.ts
@@ -15,18 +15,19 @@ type productListResponse = {
 }
 
 const actualProductCount = 34;
+const productListAPIURL = "https://automationexercise.com/api/productsList";
+const lastProductId = 43;
 
 test.describe('GetAllProductsList API tests',{tag:'@productlistapi'}, () =>{
 
 test('Verify 1st product in response has correct data',{tag: '@api'}, async ({request}) => {
-let response = await request.get("https://automationexercise.com/api/productsList");
+let response = await request.get(productListAPIURL);
 expect(response).toBeOK();
 expect(response.status()).toBe(200);
 let data: productListResponse = await response.json();
-//console.log(data);
 
 expect(data.responseCode).toBe(200);
-console.log(data.products[0]);
+//console.log(data.products[0]);
 expect(data.products[0].id).toBe(1);
 expect(data.products[0].name).toBe("Blue Top");
 expect(data.products[0].price).toBe("Rs. 500");
@@ -37,9 +38,8 @@ expect(data.products[0].category.category).toBe("Tops");
 });
 
 test('Check number of products returned is correct',{tag:'@api'}, async({request}) =>{
-const response = await request.get("https://automationexercise.com/api/productsList");
+const response = await request.get(productListAPIURL);
 await expect(response).toBeOK();
-console.log(response.headersArray());
 
 const data:productListResponse = await response.json();
 expect(data.responseCode).toBe(200);
@@ -57,7 +57,21 @@ expect(productCount).toBeGreaterThanOrEqual(actualProductCount);
 });
 
 test('Check last product in product list API', async ({request}) => {
-
+    const response = await request.get(productListAPIURL);
+    await expect(response).toBeOK();
+    expect(response.statusText()).toBe("OK");
+    console.log(response.url());
+    
+    const data:productListResponse = await response.json();
+    expect(data.responseCode).toBe(200);
+    expect(response.url()).toBe(productListAPIURL);
+    
+    const lastProduct = data.products[data.products.length-1];
+    console.log(lastProduct);
+    expect(lastProduct.id).toBe(lastProductId);
+    expect(lastProduct.name).toContain("GRAPHIC DESIGN MEN");
+    expect(lastProduct.category).toHaveProperty("usertype");
+    expect(lastProduct.category).toHaveProperty("category");
 });
 
 });
